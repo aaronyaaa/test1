11ec3d7c0b13a03bc21b801247a109e84aa66ae9
aaronyaaa <gutangaaronjhon@gmail.com>
2025-05-05 18:27:15 +0800

    aaronyaaa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          nigga 
+          ROBJAYGONZALES@GMAIL.COM 
         </p>
         <a
           className="App-link"
